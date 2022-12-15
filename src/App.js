951ac1b0ac7e4a9a1a2af527e6864827d028b040
Olaniran Micheal <olaniranmicheal2020@gmail.com>
2022-12-15 01:33:36 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,131 @@
-import logo from './logo.svg';
-import './App.css';
-
+/* eslint-disable react-hooks/exhaustive-deps */
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { searchImages } from "./Unsplash";
 function App() {
+  const [images, setImages] = useState([]);
+  const [fashionImages, setfashionImages] = useState([]);
+  const [animalImages, setAnimalImages] = useState([]);
+  const [childrenImages, setChildrenImages] = useState([]);
+  const [perfumeImages, setPerfumeImages] = useState([]);
+  let ACCESS_KEY = "agMQ6wah1H9CKRa_1ZNPGH51PX5JSNgQeX79vUeb7w8";
+  let url = `https://api.unsplash.com/photos/?client_id=${ACCESS_KEY}`;
+
+  useEffect(() => {
+    searchImages("wallpaper").then((res) => setImages(res.data.results));
+
+    getFashionImages();
+    getAnimalImages();
+    getChildrenImages();
+    getPerfumeImages();
+  }, []);
+
+  const getFashionImages = () => {
+    searchImages("fashion").then((res) => setfashionImages(res.data.results));
+  };
+
+  const getAnimalImages = () => {
+    searchImages("animal").then((res) => setAnimalImages(res.data.results));
+  };
+
+  const getChildrenImages = () => {
+    searchImages("children").then((res) => setChildrenImages(res.data.results));
+  };
+
+  const getPerfumeImages = () => {
+    searchImages("perfume").then((res) => setPerfumeImages(res.data.results));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" py-4 px-5">
+      <input
+        type="text"
+        className=" mx-2 rounded-lg border p-2"
+        placeholder="Search"
+      />
+      {/* display the images in a grid */}
+      <div className=" grid md:grid-cols-5">
+        {images.map((image) => (
+          <>
+            <div className="mx-2">
+              <img
+                key={image.id}
+                className=" rounded-lg shadow-md w-[300px] h-[200px] m-2"
+                src={image.urls.small}
+                alt={image.alt_description}
+              />
+            </div>
+          </>
+        ))}
+      </div>
+
+      <h1 className="text-4xl text-[#e42828] py-5 font-bold">Fashion</h1>
+
+      <div className=" grid md:grid-cols-5">
+        {fashionImages.map((image) => (
+          <>
+            <div className="mx-2">
+              <img
+                key={image.id}
+                className=" rounded-lg shadow-md w-[300px] h-[200px] m-2"
+                src={image.urls.small}
+                alt={image.alt_description}
+              />
+            </div>
+          </>
+        ))}
+      </div>
+
+      <h1 className="text-4xl py-5 font-bold text-[#e42828]">Animal</h1>
+
+      <div className=" grid md:grid-cols-5">
+        {animalImages.map((image) => (
+          <>
+            <div className="mx-2">
+              <img
+                key={image.id}
+                className=" rounded-lg shadow-md w-[300px] h-[200px] m-2"
+                src={image.urls.small}
+                alt={image.alt_description}
+              />
+            </div>
+          </>
+        ))}
+      </div>
+
+      <h1 className="text-4xl py-5 font-bold text-[#e42828]">Children</h1>
+
+      <div className=" grid md:grid-cols-5">
+        {childrenImages.map((image) => (
+          <>
+            <div className="mx-2">
+              <img
+                key={image.id}
+                className=" rounded-lg shadow-md w-[300px] h-[200px] m-2"
+                src={image.urls.small}
+                alt={image.alt_description}
+              />
+            </div>
+          </>
+        ))}
+      </div>
+
+      <h1 className="text-4xl text-[#e42828] py-5 font-bold">Perfume</h1>
+
+      <div className=" grid md:grid-cols-5 ">
+        {perfumeImages.map((image) => (
+          <>
+            <div className="mx-2">
+              <img
+                key={image.id}
+                className="  rounded-lg shadow-md object-cover w-[300px] h-[200px] m-2"
+                src={image.urls.small}
+                alt={image.alt_description}
+              />
+            </div>
+          </>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/Unsplash.jsx
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const unsplashApiKey = 'agMQ6wah1H9CKRa_1ZNPGH51PX5JSNgQeX79vUeb7w8';
+
+// search for images on Unsplash
+export const searchImages = query => {
+  return axios.get('https://api.unsplash.com/search/photos', {
+    params: {
+      query: query,
+      client_id: unsplashApiKey
+    }
+  });
+}
+
+// get a specific image by its ID
+export const getImageById = id => {
+  return axios.get(`https://api.unsplash.com/photos/${id}`, {
+    params: {
+      client_id: unsplashApiKey
+    }
+  });
+}
+
