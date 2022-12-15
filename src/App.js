536da8f35c46dfbf504f647979e098c5e2faca00
Olaniran Micheal <olaniranmicheal2020@gmail.com>
2022-12-15 09:43:18 +0100

    added download functions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { searchImages } from "./Unsplash";
+import { AiOutlineDownload } from "react-icons/ai";
+import { searchImages, downloadImage } from "./Unsplash";
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
   const [images, setImages] = useState([]);
   const [fashionImages, setfashionImages] = useState([]);
   const [animalImages, setAnimalImages] = useState([]);
@@ -36,6 +38,43 @@ function App() {
     searchImages("perfume").then((res) => setPerfumeImages(res.data.results));
   };
 
+  const DownloadButton = (photoId) => {
+    const handleDownload = async () => {
+      setIsLoading(true);
+
+      try {
+        // Make a request to the Unsplash API to retrieve the image by its ID
+        const response = await axios.get(
+          `https://api.unsplash.com/photos/${photoId}`,
+          {
+            params: {
+              client_id: ACCESS_KEY,
+            },
+          }
+        );
+
+        // Download the image
+        const url = response.data.urls.regular;
+        const urlResponse = await axios.get(url, {
+          responseType: "blob",
+        });
+        const fileName = `${photoId}.jpg`;
+        const link = document.createElement("a");
+        link.href = window.URL.createObjectURL(new Blob([urlResponse.data]));
+        link.setAttribute("download", fileName);
+        document.body.appendChild(link);
+        link.click();
+
+        setIsLoading(false);
+      } catch (error) {
+        // Handle any errors
+        console.error(error);
+        setIsLoading(false);
+      }
+    };
+
+    return handleDownload();
+  };
   return (
     <div className=" py-4 px-5">
       <input
@@ -46,16 +85,19 @@ function App() {
       {/* display the images in a grid */}
       <div className=" grid md:grid-cols-5">
         {images.map((image) => (
-          <>
-            <div className="mx-2">
-              <img
-                key={image.id}
-                className=" rounded-lg shadow-md w-[300px] h-[200px] m-2"
-                src={image.urls.small}
-                alt={image.alt_description}
-              />
-            </div>
-          </>
+          <div key={image.id} className="mx-2">
+            <img
+              key={image.id}
+              className=" rounded-lg shadow-md w-[300px] h-[200px] m-2"
+              src={image.urls.small}
+              alt={image.alt_description}
+            />
+            <AiOutlineDownload
+              onClick={() => DownloadButton(image.id)}
+              size={28}
+              className=" text-black cursor-pointer absolute top-[] mt-[-38px] md:mt-[-35px] ml-[236px] md:ml-[218px] bg-white "
+            />
+          </div>
         ))}
       </div>
 
@@ -63,16 +105,19 @@ function App() {
 
       <div className=" grid md:grid-cols-5">
         {fashionImages.map((image) => (
-          <>
-            <div className="mx-2">
-              <img
-                key={image.id}
-                className=" rounded-lg shadow-md w-[300px] h-[200px] m-2"
-                src={image.urls.small}
-                alt={image.alt_description}
-              />
-            </div>
-          </>
+          <div key={image.id} className="mx-2">
+            <img
+              key={image.id}
+              className=" rounded-lg shadow-md w-[300px] h-[200px] m-2"
+              src={image.urls.small}
+              alt={image.alt_description}
+            />
+            <AiOutlineDownload
+              onClick={() => DownloadButton(image.id)}
+              size={28}
+              className=" text-black cursor-pointer absolute top-[] mt-[-38px] md:mt-[-35px] ml-[236px] md:ml-[218px] bg-white "
+            />
+          </div>
         ))}
       </div>
 
@@ -80,16 +125,19 @@ function App() {
 
       <div className=" grid md:grid-cols-5">
         {animalImages.map((image) => (
-          <>
-            <div className="mx-2">
-              <img
-                key={image.id}
-                className=" rounded-lg shadow-md w-[300px] h-[200px] m-2"
-                src={image.urls.small}
-                alt={image.alt_description}
-              />
-            </div>
-          </>
+          <div key={image.id} className="mx-2">
+            <img
+              key={image.id}
+              className=" rounded-lg shadow-md w-[300px] h-[200px] m-2"
+              src={image.urls.small}
+              alt={image.alt_description}
+            />
+            <AiOutlineDownload
+              onClick={() => DownloadButton(image.id)}
+              size={28}
+              className=" text-black cursor-pointer absolute top-[] mt-[-38px] md:mt-[-35px] ml-[236px] md:ml-[218px] bg-white "
+            />
+          </div>
         ))}
       </div>
 
@@ -97,16 +145,19 @@ function App() {
 
       <div className=" grid md:grid-cols-5">
         {childrenImages.map((image) => (
-          <>
-            <div className="mx-2">
-              <img
-                key={image.id}
-                className=" rounded-lg shadow-md w-[300px] h-[200px] m-2"
-                src={image.urls.small}
-                alt={image.alt_description}
-              />
-            </div>
-          </>
+          <div key={image.id} className="mx-2">
+            <img
+              key={image.id}
+              className=" rounded-lg shadow-md w-[300px] h-[200px] m-2"
+              src={image.urls.small}
+              alt={image.alt_description}
+            />
+            <AiOutlineDownload
+              onClick={() => DownloadButton(image.id)}
+              size={28}
+              className=" text-black cursor-pointer absolute top-[] mt-[-38px] md:mt-[-35px] ml-[236px] md:ml-[218px] bg-white "
+            />
+          </div>
         ))}
       </div>
 
@@ -114,16 +165,19 @@ function App() {
 
       <div className=" grid md:grid-cols-5 ">
         {perfumeImages.map((image) => (
-          <>
-            <div className="mx-2">
-              <img
-                key={image.id}
-                className="  rounded-lg shadow-md object-cover w-[300px] h-[200px] m-2"
-                src={image.urls.small}
-                alt={image.alt_description}
-              />
-            </div>
-          </>
+          <div key={image.id} className="mx-2">
+            <img
+              key={image.id}
+              className="  rounded-lg shadow-md object-cover w-[300px] h-[200px] m-2"
+              src={image.urls.small}
+              alt={image.alt_description}
+            />
+            <AiOutlineDownload
+              onClick={() => DownloadButton(image.id)}
+              size={28}
+              className=" text-black cursor-pointer absolute top-[] mt-[-38px] md:mt-[-35px] ml-[236px] md:ml-[218px] bg-white "
+            />
+          </div>
         ))}
       </div>
     </div>
--- src/Unsplash.jsx
+++ src/Unsplash.jsx
@@ -1,23 +1,42 @@
 import axios from "axios";
 
-const unsplashApiKey = 'agMQ6wah1H9CKRa_1ZNPGH51PX5JSNgQeX79vUeb7w8';
+const unsplashApiKey = "agMQ6wah1H9CKRa_1ZNPGH51PX5JSNgQeX79vUeb7w8";
 
 // search for images on Unsplash
-export const searchImages = query => {
-  return axios.get('https://api.unsplash.com/search/photos', {
+export const searchImages = (query) => {
+  return axios.get("https://api.unsplash.com/search/photos", {
     params: {
       query: query,
-      client_id: unsplashApiKey
-    }
+      client_id: unsplashApiKey,
+    },
   });
-}
+};
+
+
+
+//download image
+// export async function downloadImage(photoId)  {
+//   const imgUrl = axios.get("https://api.unsplash.com/search/photos")
+//   const getImage = await imgUrl.photos
+//   getImage.getPhoto(photoId).then((response) => response.blob())
+//     .then((blob) => {
+//       const a = document.createElement("a");
+//       a.href = URL.createObjectURL(blob);
+//       a.download = "photo.jpg";
+//       a.click();
+//       console.log("they just click me now o")
+//     });
+    
+    
+// }
+
+
 
 // get a specific image by its ID
-export const getImageById = id => {
+export const getImageById = (id) => {
   return axios.get(`https://api.unsplash.com/photos/${id}`, {
     params: {
-      client_id: unsplashApiKey
-    }
+      client_id: unsplashApiKey,
+    },
   });
-}
-
+};
