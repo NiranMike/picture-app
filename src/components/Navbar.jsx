eacbd466ca024646e5f8a9b86220d65983107b37
Olaniran Micheal <olaniranmicheal2020@gmail.com>
2022-12-24 11:55:29 +0100

    changes in button and background

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   
   return (
-    <div className=' px-7 mb-3 py-3 absolute z-10 bg-[#90909013] w-full'>
+    <div className=' px-7 mb-3 py-3 absolute z-10 bg-[#90909013] '>
         <Link to={'/'}><h1 className=' text-black bg-[#90909000] font-bold text-xl'>Picture App</h1></Link>
     </div>
   )
--- src/pages/SearchPage/HeroSearch.jsx
+++ src/pages/SearchPage/HeroSearch.jsx
@@ -7,30 +7,32 @@ const HeroSearch = ({querySearch, searchedImage}) => {
     return (
         <div className='py-10 px-3'>
             <h1 className="text-4xl mx-4 text-center text-[#000] py-5 font-bold">Here are the results for "{querySearch}"</h1>
-            <div className=" justify-center grid mm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
+            <div className=" justify-center grid gap-5 mm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
                 {
                 searchedImage.map((image) => {
-                    
+
                     return (
                         <div key={
                                 image.id
                             }
-                            className="mx-2">
+                            className="mx-0 ">
                             <img key={
                                     image.id
                                 }
-                                className=" object-cover shadow-md w-[300px] h-[300px] mm:h-[200px] m-2"
+                                className=" rounded-md object-cover shadow-md w-full h-[300px] mm:h-[200px] 2xl:h-[320px]"
                                 src={
                                     image.urls.small
                                 }
                                 alt={
                                     image.alt_description
                                 }/>
-                            <AiOutlineDownload onClick={
-                                    () => DownloadButton(image.id)
-                                }
-                                size={28}
-                                className=" text-white cursor-pointer absolute top-[] bg-black mt-[-36px] md:mt-[-36px] ml-[260px] ml:ml-[165px] mm:ml-[139px] md:ml-[212px] lg:ml-[164px] xl:ml-[232px] 2xl:ml-[279px] "/>
+                            <div className='w-full flex justify-end'>
+                                <AiOutlineDownload onClick={
+                                        () => DownloadButton(image.id)
+                                    }
+                                    size={28}
+                                    className=" text-white cursor-pointer  top-[] bg-transparent mt-[-29px] md:mt-[-29px] ml-[245px] ml:ml-[155px] mm:ml-[132px] md:ml-[206px] lg:ml-[160px] xl:ml-[226px] 2xl:ml-[279px] "/>
+                            </div>
                         </div>
                     )
                 })
